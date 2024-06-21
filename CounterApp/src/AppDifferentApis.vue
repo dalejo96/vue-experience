e2cc33c6295e8705bf120ed75c6b402ac7b47e63
dalejo96 <dalejo_1996@hotmail.com>
2024-06-20 21:59:39 -0500

    use composition and options api

---
+++ CounterApp/src/AppDifferentApis.vue
@@ -0,0 +1,58 @@
+<!--COMPOSITION API-->
+<!-- <script setup>
+import { ref } from "vue";
+const count = ref(0);
+
+const addCount = () => {
+  count.value = count.value + 1;
+};
+
+const subtractCount = () => {
+  count.value = count.value - 1;
+};
+</script> -->
+
+<!--OPTIONS API-->
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    addToCount() {
+      this.count = this.count + 1;
+    },
+    subtractFromCount() {
+      this.count = this.count - 1;
+    },
+  },
+};
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>The current count is...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="subtractCount()">-</button>
+      <button @click="addCount()">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: aliceblue;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+div {
+  text-align: center;
+}
+</style>
