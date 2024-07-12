08113b996a73b04a71783b20e7c1eb8ac132011a
dalejo96 <dalejo_1996@hotmail.com>
2024-07-11 22:36:22 -0500

    scaffolding Show app

---
+++ ShowApp/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import GotCards from "./components/GoTCard.vue";
+</script>
+
+<template>
+  <main>
+    <h1>Hero</h1>
+    <GotCards />
+  </main>
+</template>
---
+++ ShowApp/src/components/GoTCard.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>Game of Thrones Cards</h1>
+  </div>
+</template>
