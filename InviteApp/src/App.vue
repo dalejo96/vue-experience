fdffbed21e2f7559d9b262e93b50cc206cdfe9cd
dalejo96 <dalejo_1996@hotmail.com>
2024-07-16 22:12:02 -0500

    managing types with State

--- InviteApp/src/App.vue
+++ InviteApp/src/App.vue
@@ -1,14 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+enum Gender {
+  MALE,
+  FEMALE,
+}
+
+const name = ref<string>("");
+const gender = ref(Gender.MALE);
+</script>
 
 <template>
   <main>
     <div>
       <h1>People Invited to My Party</h1>
       <div class="input-container">
-        <input type="text" placeholder="Name..." />
-        <select>
-          <option value="">Male</option>
-          <option value="">Female</option>
+        <input type="text" placeholder="Name..." v-model="name" />
+        <select v-model="gender">
+          <option :value="Gender.MALE">Male</option>
+          <option :value="Gender.FEMALE">Female</option>
         </select>
       </div>
     </div>
